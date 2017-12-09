6c8ff029bcec31ceca513aa946581e6b49128c6d
lucio-lpf <lucio.lpf@gmail.com>
2017-12-09 18:16:42 -0200

    ButtonNode

--- Frogger/Model/ButtonNode.swift
+++ Frogger/Model/ButtonNode.swift
@@ -1,34 +1,74 @@
 //
-//  ButtonNode.swift
+//  PlayerNode.swift
 //  Frogger
 //
-//  Created by Rodrigo Franzoi Scroferneker on 09/12/17.
+//  Created by Lúcio Pereira Franco on 05/12/2017.
 //  Copyright © 2017 Rodrigo Franzoi Scroferneker. All rights reserved.
 //
 
-//import SpriteKit
-//
-//protocol ButtonDelegate {
-//    func didSelect
-//}
-//
-//class ButtonNode: SKSpriteNode {
-//    
-//    var delegate : ButtonDelegate
-//    
-//    init(texture: SKTexture?, size: CGSize, name: String) {
-//        super.init(texture: texture, color: .clear, size: size)
-//        self.name = name
-//    }
-//    
-//    func select() {
-//        let action
-//        self.run()
-//    }
-//    
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//}
 
+import SpriteKit
+
+protocol ButtonDelegate {
+    func setSelectedButton(buttonNode: ButtonNode)
+    func setDeselectedButton(buttonNode: ButtonNode)
+}
+
+class ButtonNode: SKSpriteNode {
+    
+    private var selected : Bool
+    private var delegate : ButtonDelegate!
+    
+    init(texture: SKTexture?, size: CGSize, scene:ButtonDelegate) {
+        self.selected = false
+        self.delegate = scene
+        super.init(texture: texture, color: .cyan, size: size)
+        self.isUserInteractionEnabled = true
+        
+    }
+    
+    func selectButton() {
+        self.selected = !self.selected
+        if selected {
+            self.select()
+        } else {
+            self.deselect()
+        }
+    }
+    
+    private func select() {
+        self.selected = true
+        self.color = .yellow
+        self.delegate.setSelectedButton(buttonNode: self)
+        
+        let action = SKAction.scale(to: 0.95, duration: 0.1)
+        run(action)
+        
+    }
+    
+    public func deselect() {
+        self.selected = false
+        self.color = .cyan
+        self.delegate.setDeselectedButton(buttonNode: self)
+        
+        let action = SKAction.scale(to: 1, duration: 0.1)
+        run(action)
+    }
+    
+    public func isSelected() -> Bool {
+        return selected
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.select()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.deselect()
+    }
+}
